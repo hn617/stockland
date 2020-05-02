52c2b24ce505cfbbaa946d8ece2696c8489907b7
Hossein Naseri <hossein.naseri@farmersedge.ca>
2020-05-02 15:59:50 -0400

    stockland

---
+++ stockland/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for stockland project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+from dotenv import load_dotenv
+from django.core.wsgi import get_wsgi_application
+load_dotenv('.env')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stockland.settings")
+application = get_wsgi_application()
